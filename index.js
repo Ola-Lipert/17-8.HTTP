3ab9189cb039de7f214d5c8f54de075aa0310b4f
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-01-26 23:28:38 +0100

    Add index.js

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 'use strict';
 
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>');
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            response.writeHead(200, {'Content-type':'text/html'});
+            response.write(data);
             response.end();
+        });
     } else {
+        fs.readFile('./cat.jpg', function(err, data) {
+            response.writeHead(404, {'Content-type':'image/jpg'});
+            response.write(data);
             response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
             response.end();
+        })
     }
 });
-
-server.listen(8080);
+server.listen(9000);
 
 /*
 var http = require('http');
